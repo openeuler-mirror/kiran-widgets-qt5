bc726c18aad6d194d16219fc2d6a1578826b56c6
liuxinhao <liuxinhao@kylinsec.com.cn>
2024-03-28 19:38:23 +0800

    fix(combobox): Fix incorrect settings for dropdown proxy
    - 修复错误的设置下拉框代理的问题

--- src/kiran-style/style.cpp
+++ src/kiran-style/style.cpp
@@ -1,17 +1,17 @@
 /**
- * Copyright (c) 2020 ~ 2021 KylinSec Co., Ltd. 
+ * Copyright (c) 2020 ~ 2021 KylinSec Co., Ltd.
  * kiranwidgets-qt5 is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2. 
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2 
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, 
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, 
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
- * See the Mulan PSL v2 for more details.  
- * 
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
  * Author:     liuxinhao <liuxinhao@kylinos.com.cn>
  */
- 
+
 #include "style.h"
 #include "style-detail-fetcher.h"
 
@@ -850,9 +850,9 @@ void Style::polish(QWidget *widget)
 
     if (QComboBox *comboBox = qobject_cast<QComboBox *>(widget)) {
         QAbstractItemView *itemView(comboBox->view());
-        if (itemView && itemView->itemDelegate() && 
+        if (itemView && itemView->itemDelegate() &&
            (itemView->itemDelegate()->inherits("QComboBoxDelegate") || itemView->itemDelegate()->inherits("QComboMenuDelegate")) ) {
-            comboBox->setItemDelegate(new ComboBoxItemDelegate(comboBox,comboBox));
+            itemView->setItemDelegate(new ComboBoxItemDelegate(comboBox,comboBox));
         }
     }
 
