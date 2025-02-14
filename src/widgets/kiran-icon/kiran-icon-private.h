49d44a32bffce399b629ae0d73dff4b1c54c881b
yuanxing <yuanxing@kylinsec.com.cn>
2025-02-14 09:44:14 +0800

    fix(KiranIcon):add license in kiran icon files
    - 在KiranIcon类文件中添加license信息

--- src/widgets/kiran-icon/kiran-icon-private.h
+++ src/widgets/kiran-icon/kiran-icon-private.h
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2020 ~ 2021 KylinSec Co., Ltd.
- * kiranwidgets-qt5 is licensed under Mulan PSL v2.
+ * Copyright (c) 2020 ~ 2025 KylinSec Co., Ltd.
+ * kiran-widgets-qt5 is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *          http://license.coscl.org.cn/MulanPSL2
@@ -9,7 +9,7 @@
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  *
- * Author:     yuanxing <yuanxing@kylinos.com.cn>
+ * Author:     yuanxing <yuanxing@kylinsec.com.cn>
  */
 
 #ifndef KIRANICON_PRIVATE_H
